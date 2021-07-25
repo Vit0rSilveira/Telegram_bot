e25608797d7089273e3f7cecf9b4b3e9beb686f6
vit0rSilveira <vitor.silveira98@gmail.com>
2021-07-25 17:08:02 -0300

    Adicionando funcao

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from telegram.ext import Updater, CommandHandler, commandhandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+
+from telegram.ext.messagequeue import queuedmessage
 import dontpad
 import os
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv('ignore/.env')
 
 TOKEN = os.getenv("TOKEN_BOT")
 CHAT = os.getenv("CHAT_ID")
+
+guardarDia = 0
+quandoMandar = 0
 
 def informacao(update, context):
     s = """Digite /provas para ver as datas das provas.
@@ -46,6 +52,29 @@ def addExercicios(update, context):
         mensagem = "Para editar clique [aqui](http://dontpad.com/exerciciosBCC021)"
         context.bot.sendMessage(chat_id = chatId, text = mensagem, parse_mode = "MarkdownV2")
 
+def mandarPeriodicamente(update, context):
+    global guardarDia
+    global quandoMandar
+    #print("Felps pirocudo\n")
+
+    if guardarDia == quandoMandar:
+        data = str(datetime.datetime.now().today()).split(" ")[0]
+        guardarDia = int(data.split("-")[2])
+
+        quandoMandar = guardarDia + 1
+        if quandoMandar > 30:
+            quandoMandar -= 30
+
+        mensagem = dontpad.read("provasBCC021")
+        context.bot.sendMessage(chat_id = CHAT, text = mensagem, disable_notification = True)
+
+        mensagem = dontpad.read("trabalhosBCC021")
+        context.bot.sendMessage(chat_id = CHAT, text = mensagem, disable_notification = True)
+
+        mensagem = dontpad.read("exerciciosBCC021")
+        context.bot.sendMessage(chat_id = CHAT, text = mensagem, disable_notification = True)
+
+
 def main():
     logger = logging.getLogger(__name__)
     logging.basicConfig(
@@ -59,6 +88,7 @@ def main():
     dp.add_handler(CommandHandler("provas", addProvas))
     dp.add_handler(CommandHandler("trabalhos", addTrabalhos))
     dp.add_handler(CommandHandler("runcodes", addExercicios))
+    dp.add_handler(MessageHandler(Filters.text, mandarPeriodicamente))
 
     updater.start_polling()
     logging.info("=== Bot running! ===")
